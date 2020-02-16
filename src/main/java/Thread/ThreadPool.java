0c00b433a85d408a2b94329d0d76ebf681a8519d
monalisali <monalisali@hotmail.com>
2020-02-16 16:03:12 +0800

    线程池，submit,get()

---
+++ src/main/java/Thread/ThreadPool.java
@@ -0,0 +1,20 @@
+package Thread;
+
+import java.util.concurrent.*;
+
+public class ThreadPool {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        final Future<?> future = threadPool.submit(() -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("我结束工作啦！");
+        });
+        System.out.println("我把工作提交啦！");
+        System.out.println(future.get());
+        System.out.println("我提交的工作做完啦！");
+    }
+}
